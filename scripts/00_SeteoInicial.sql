dd48d402b50f4956892776d60a21adab8d407f0b
Victor <99147005+victor-POL@users.noreply.github.com>
2024-10-26 23:49:54 -0300

    Cambio script seteo inicial

--- scripts/00_SeteoInicial.sql
+++ scripts/00_SeteoInicial.sql
@@ -9,20 +9,23 @@
 -- INTEGRANTE_3: TODO: DATOS
 -- INTEGRANTE_4: TODO: DATOS
 ---------------------------------------------------------------------------------------------------------------------
-CREATE SCHEMA negocio;
+DROP SCHEMA IF EXISTS negocio CASCADE;
+CREATE SCHEMA IF NOT EXISTS negocio;
 -- ELIMINO CUALQUIER TABLA DE LA DDBB
 DROP TABLE IF EXISTS negocio.sucursal CASCADE;
 DROP TABLE IF EXISTS negocio.ciudad;
 DROP TABLE IF EXISTS negocio.sucursal_horario;
-DROP TABLE IF EXISTS negocio.empleado;
+DROP TABLE IF EXISTS negocio.empleado CASCADE;
 DROP TABLE IF EXISTS negocio.cargo_laboral;
 DROP TABLE IF EXISTS negocio.turno_laboral;
 DROP TABLE IF EXISTS negocio.producto CASCADE;
 DROP TABLE IF EXISTS negocio.categoria_producto;
 DROP TABLE IF EXISTS negocio.clasfificacion_categoria_producto;
-DROP TABLE IF EXISTS negocio.tipo_cliente;
-DROP TABLE IF EXISTS negocio.medio_pago;
+DROP TABLE IF EXISTS negocio.tipo_cliente CASCADE;
+DROP TABLE IF EXISTS negocio.medio_pago CASCADE;
 DROP TABLE IF EXISTS negocio.extra_info_producto;
+DROP TABLE IF EXISTS negocio.cliente CASCADE;
+DROP TABLE IF EXISTS negocio.venta CASCADE;
 -- FUNCIONES PARA LAS TABLAS
 CREATE OR REPLACE FUNCTION negocio.validar_cuil(cuil TEXT) RETURNS BOOLEAN AS $$ BEGIN -- TODO: Implementar la función que valide un CUIL
     RETURN true;
@@ -32,4 +35,4 @@ $$ LANGUAGE plpgsql;
 -- https://www.postgresql.org/docs/current/collation.html
 -- https://dba.stackexchange.com/questions/299477/collation-for-accent-insensitive-comparison-on-postgres
 -- https://help.scriptcase.net/portal/en/kb/articles/how-to-use-the-ignore-accent-option#Enabling_extension
-CREATE EXTENSION unaccent;
+CREATE EXTENSION IF NOT EXISTS unaccent;
